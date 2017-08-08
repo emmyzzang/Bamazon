922d719ab77099bc7608f26c97048fcff1c2761c
Emmy Emme <masteremmy.js@gmail.com>
2017-08-08 01:12:43 -0400

    Init

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,25 @@
 
-const inquirer = require("inquirer");
+// NODE DRIVERS ZOOM ZOOM 
 const mysql = require("mysql"); 
+const inquirer = require("inquirer");
+const accounting = require("accounting"); 
+const chalk = require("chalk");
+const Table = require('cli-table');
+
+// MYSQL CONNECT 
+let connection = mysql.createConnection({
+	host: 'localhost', 
+	port: 3306,
+	user: 'root', 
+	password: '', 
+	database: 'bamazon_db'
+}); 
+
+connection.connect(function(err) {
+	if (err) throw err; 
+	console.log('Your database requires additional pylons!');
+	start(); 
+}); 
+
+let max, col = ['Item ID', 'Product Name', 'Price'];
+
